724091bb2ce9cde08f8c82f3321db79942953727
Raven <ravend2013@gmail.com>
2025-09-19 14:56:15 +0100

    feat: now generates report

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import affected from './affected.json' with {type: 'json'};
 
+const __dirname = import.meta.dirname;
+
 if (typeof process.argv[2] === 'undefined') {
   throw 'Please enter at least one directory';
 }
-const dirs = process.argv[2].split(',');
+const directories = process.argv[2].split(',');
 const excludeList = ['node_modules', '.git', 'venv', 'other-files', '__pycache__'];
 if (typeof process.argv[3] !== 'undefined') excludeList.concat(process.argv[3]?.split(','));
 
@@ -17,20 +19,24 @@ const fullAffectedList = affected.flatMap((lib) => {
   return versions.map((v) => `${name}-${v}`);
 });
 
-const readSingleDir = async (dir) => {
+const readSingleDir = (dir, report, initDir) => {
+  if (typeof initDir === 'undefined') initDir = dir;
   fs.readdirSync(dir).forEach((file) => {
     const curDir = path.join(dir, file);
     if (!excludeList.some((ex) => file === ex)) {
       if (fs.lstatSync(curDir).isDirectory()) {
-        readSingleDir(curDir);
+        readSingleDir(curDir, report, initDir);
       } else if (fs.lstatSync(curDir).isFile() && (file === 'yarn.lock' || file === 'package-lock.json')) {
         try {
           const fileContents = fs.readFileSync(curDir, {encoding: 'utf-8'});
-          const isSafe = fullAffectedList.every((lib) => {
-            return !fileContents.includes(lib);
-          });
-          if (!isSafe) console.log(`${curDir} is using affected library: ${lib}`);
-          console.log(fileContents);
+          const isSafe = fullAffectedList.every((lib) => !fileContents.includes(lib));
+          if (!isSafe) {
+            const vulnerabilities = [];
+            fullAffectedList.forEach((lib) => {
+              if (fileContents.includes(lib)) vulnerabilities.push(lib);
+            });
+            report[initDir].affected.push({directory: curDir, vulnerabilities});
+          } else report[initDir].safe.push({directory: curDir});
         } catch (err) {
           console.error('ERROR', err);
         }
@@ -39,4 +45,14 @@ const readSingleDir = async (dir) => {
   });
 };
 
-await readSingleDir(dirs[0]);
+const readDirs = (dirs) => {
+  const report = {};
+  dirs.forEach((dir) => {
+    report[dir] = {affected: [], safe: []};
+    readSingleDir(dir, report);
+  });
+  fs.writeFileSync('report.json', JSON.stringify(report));
+  console.log(`Report written to ${__dirname}/report.json`);
+};
+
+readDirs(directories);
